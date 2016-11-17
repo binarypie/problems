8635bc56a6c16fb1a3ef405bc347be82e783a2fe
Amanda Truong <amt140130@utdallas.edu>
2016-11-16 21:14:07 -0600

    fixed capital function

--- PigLatinOld.js
+++ PigLatinOld.js
@@ -1,4 +1,5 @@
-var str = "Hello darkness. Can't Wo'man Whodatbe.";
+//var str = "Hello darkness. Can't Wo'man Whodatbe.";
+var str = "HeLLo DarKnesS";
 var wordArray = [];
 var finalString = "";
 wordArray = str.split(" ");
@@ -9,6 +10,7 @@ for (var i = 0; i < wordArray.length; i++) {
     var exclamationStack = [];
     var questionMarkStack = [];
     var periodStack = [];
+    var apostStack = [];
     var word = [];
     word = wordArray[i];
 
@@ -20,17 +22,22 @@ for (var i = 0; i < wordArray.length; i++) {
     if (word.toLowerCase() == "way") {
         finalString += word + " ";
     }
-    for (var j = 0; j < word.length; j++) {
+    for (var j = 0; j < word.length; j++) { //Wo'man   Oman'way
         console.log("for loop run" + " " + word);
-        if (word[j] == word[j].toUpperCase())
+        if (word[j] == null) {
+          continue;
+        }
+        if (word[j] == word[j].toUpperCase()) //cstack pushed 0
             cStack.push(j);
         if (word[j] == "!") {
             exclamationStack.push(j);
             word = word.replace("!", "");
+            var origWord = word;
         }
         if (word[j] == "?") {
             questionMarkStack.push(j);
             word = word.replace("?", "");
+            var origWord = word;
         }
         if (word[j] == ".") {
             console.log("period check " + word[j])
@@ -39,12 +46,17 @@ for (var i = 0; i < wordArray.length; i++) {
             console.log("period replace " + word)
             var origWord = word;
         }
+        if (word[j] == "'") { //apoststack pushed 2
+            apostStack.push(j);
+            word = word.replace("'", ""); //Woman
+            var origWord = word;
+        }
         console.log("at end of for loop " + word);
         console.log("period stack" + periodStack);
         console.log("capital stack " + cStack);
     }
 
-    word = word.toLowerCase();
+    word = word.toLowerCase(); //woman
 
     if (word[0] == 'a' || word[0] == 'e' || word[0] == 'i' || word[0] == 'o' || word[0] == 'u') {
         var ending = "way";
@@ -54,23 +66,15 @@ for (var i = 0; i < wordArray.length; i++) {
         word += ending;
         console.log(word);
     } else {
-        var ending = word.substr(0, 1) + "ay";
+        var ending = word.substr(0, 1) + "ay"; //way
         console.log("word[0] " + word[0])
-        word = word.substr(1, word.length);
+        word = word.substr(1, word.length); //oman
         console.log("word " + word);
-        word += ending;
+        word += ending; //omanway
         console.log(word);
     }
-
-    for (var x = 0; x < cStack.length; x++) {
-        var count = cStack.pop();
-        console.log("cstack pop " + count);
-        word[count] = word[count].toUpperCase();
-        word = word.substr(0, count) + word[count].toUpperCase() + word.substr(count + 1, word.length);
-        console.log(word[count].toUpperCase());
-        console.log(word[count]);
-        console.log(word);
-    }
+    var clength = cStack.length;
+    word = capitalize(word, cStack, clength);
 
     for (var y = 0; y < periodStack.length; y++) {
         var pos = periodStack.pop();
@@ -89,3 +93,21 @@ for (var i = 0; i < wordArray.length; i++) {
 }
 
 console.log(finalString);
+
+
+
+function capitalize(word, stack, length) { //omanway stack with 0 and 3
+  for (var i = 0; i < length; i++) {
+    var pos = stack.pop();
+    if (pos == 0) {
+      word = word.substr(0, 1).toUpperCase() + word.substr(1);
+    }
+    if (pos == word.length - 1) {
+      word = word.substr(0, word.length - 1) + word.substr(word.length - 1).toUpperCase();
+    }
+    if (pos > 0 && pos < word.length-1) {
+		word = word.substr(0, pos) + word[pos].toUpperCase() + word.substr(pos+1);
+    }
+  }
+  return word;
+}
