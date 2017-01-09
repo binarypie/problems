03d291c4bcd33249bc17dd30ab87f03fa37fa83c
Phil Gore <philgore@u.boisestate.edu>
2017-01-08 22:23:37 -0700

    Fixing go solution for efficiency and correctness

--- Fibonacci.go
+++ Fibonacci.go
@@ -1,58 +1,31 @@
-package main
+package fibonacci
 
-import (
-	"fmt"
-	"strconv"
-)
-
-const MAX_NUMBER = 93
-
-func main() {
-	numInts := getInput()
+func Fibonacci(numInts int) []int64 {
 	var count int
 	input1 := int64(0)
 	input2 := int64(1)
 
-	//Check for initial cases.
-	if numInts >= 1 {
-		count++
-		fmt.Printf("%d, ", 0)
-	}
-	if numInts >= 2 {
-		count++
-		fmt.Printf("%d, ", 1)
-	}
-	fibonacci(&input1, &input2, numInts, &count)
+	return getSeq(&input1, &input2, numInts, count)
 }
 
-func getInput() int {
-	// Any above 93, and longs start to wrap around to the negatives.
-	fmt.Println("Please input the number of fibonacci numbers you want: (0-93)")
-	var input string
-	fmt.Scanln(&input)
-	numInts, err := strconv.ParseInt(input, 10, 32)
-	if err != nil {
-		fmt.Println("Could not parse int, try again!\n" + err.Error())
-		return getInput()
-	} else if numInts > MAX_NUMBER {
-		fmt.Println("Number too large, try again!\n")
-		return getInput()
-	} else if numInts < 0 {
-		fmt.Println("Number must be positive, try again!\n")
-		return getInput()
-	}
-	return int(numInts)
-}
-
-func fibonacci(input1 *int64, input2 *int64, numInts int, count *int) {
-	if numInts > *count {
-		*count++
-		retVal := *input1 + *input2
+func getSeq(input1 *int64, input2 *int64, numInts int, count int) []int64 {
+	var sequence []int64
+	var retVal int64
+	if (count == 0) && (numInts >= 1) {
+		count++
+	} else if (count == 1) && (numInts >= 2) {
+		retVal = 1
+		count++
+	} else if count > 1 {
+		count++
+		retVal = *input1 + *input2
 		input1 = input2
 		input2 = &retVal
-		fmt.Printf("%d, ", retVal)
-		fibonacci(input1, &retVal, numInts, count)
+	}
+	if numInts >= count {
+		sequence = append(sequence, retVal)
+		return append(sequence, getSeq(input1, &retVal, numInts, count)...)
 	} else {
-		fmt.Printf("\b\b\n")
+		return nil
 	}
 }
