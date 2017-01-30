1e8235030b1d16c29e528b2b37788c63c9c23127
Unknown <bldkbrooks@gmail.com>
2017-01-30 00:43:01 -0600

    Logs
    Implemented and tested the LogReader and LogItem. Updated my notes.

--- solutions/logs.py
+++ solutions/logs.py
@@ -1,12 +1,57 @@
 from utility import FileReader
+import re
 
 
-# class LogReader:
-#
-#
-# class LogItem:
-#
-#
+class LogReader:
+    def __init__(self, file_reader=FileReader()):
+        self._file_reader = file_reader
+
+    def read_log(self, filename):
+        file_contents = self._file_reader.read_file(filename)
+        log_items = []
+
+        for entry in file_contents.split('\n'):
+            log_items.append(LogItem(entry))
+
+        return log_items
+
+
+class LogItem:
+    def __init__(self, log_entry):
+        expression = r'(?P<ip>[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+).*\[(?P<date>[0-9]{2}/[A-Za-z]{3}/[0-9]{4}):(?P<time>[' \
+                     r'0-9]{2}:[0-9]{2}:[0-9]{2}).*] "[A-Z]* /?(?P<file>.*\..*) [A-Z]+/[0-9]+\.[0-9]+" [0-9]+ [0-9]+ ' \
+                     r'"(?P<referrer>.*)" '
+        p = re.compile(expression)
+
+        match = p.match(log_entry)
+
+        if match:
+            self._ip = match.group("ip")
+            self._date = match.group("date")
+            self._time = match.group("time")
+            self._file = match.group("file")
+            self._referrer = match.group("referrer")
+
+        self._entry = log_entry
+
+    def ip(self):
+        return self._ip
+
+    def date(self):
+        return self._date
+
+    def time(self):
+        return self._time
+
+    def file(self):
+        return self._file
+
+    def referrer(self):
+        return self._referrer
+
+    def entry(self):
+        return self._entry
+
 # class LogSearch:
 
 
--- solutions/test_logs.py
+++ solutions/test_logs.py
@@ -1,12 +1,118 @@
 import unittest
+from logs import *
 
-# class LogReaderTests:
-#
-#
-# class LogItemTests:
-#
-#
-# class LogSearchTests:
+
+class FakeFileReader:
+    def __init__(self, echo_text):
+        self._echo_text = echo_text
+
+    def read_file(self, filename):
+        return self._echo_text
+
+
+class LogReaderTests(unittest.TestCase):
+    def test_read_log_runs(self):
+        reader = LogReader(file_reader=FakeFileReader("test"))
+
+        reader.read_log("")
+
+    def test_read_log_returns_list(self):
+        reader = LogReader(file_reader=FakeFileReader("test"))
+
+        actual = reader.read_log("")
+
+        self.assertIsInstance(actual, list)
+
+    def test_read_log_returns_one(self):
+        reader = LogReader(file_reader=FakeFileReader("test"))
+        expected = 1
+
+        actual = len(reader.read_log(""))
+
+        self.assertEqual(expected, actual)
+
+    def test_read_log_returns_three(self):
+        reader = LogReader(file_reader=FakeFileReader("test\ntest\ntest"))
+        expected = 3
+
+        actual = len(reader.read_log(""))
+
+        self.assertEqual(expected, actual)
+
+    def test_read_log_correct_entry(self):
+        reader = LogReader(file_reader=FakeFileReader("test"))
+
+        actual = reader.read_log("")[0].entry()
+
+        self.assertEqual(actual, "test")
+
+    def test_read_log_correct_entries(self):
+        reader = LogReader(file_reader=FakeFileReader("one\ntwo\nthree"))
+        expected = "three"
+
+        actual = reader.read_log("")[2].entry()
+
+        self.assertEqual(expected, actual)
+
+
+class LogItemTests(unittest.TestCase):
+    def setUp(self):
+        self.test_log_string = '111.1.1.1 [01/Jan/2017:00:00:00] ' \
+                               '"TEST /TEST.test HTTP/1.1" 200 0 "http://test.com/tester" '
+
+    def test_object_builds(self):
+        LogItem("")
+
+    def test_contains_log_entry(self):
+        log_item = LogItem("")
+        expected = ""
+
+        actual = log_item.entry()
+
+        self.assertEqual(expected, actual)
+
+    def test_contains_ip(self):
+        log_item = LogItem(self.test_log_string)
+        expected = "111.1.1.1"
+
+        actual = log_item.ip()
+
+        self.assertEqual(expected, actual)
+
+    def test_contains_date(self):
+        log_item = LogItem(self.test_log_string)
+        expected = "01/Jan/2017"
+
+        actual = log_item.date()
+
+        self.assertEqual(expected, actual)
+
+    def test_contains_time(self):
+        log_item = LogItem(self.test_log_string)
+        expected = "00:00:00"
+
+        actual = log_item.time()
+
+        self.assertEqual(expected, actual)
+
+    def test_contains_file(self):
+        log_item = LogItem(self.test_log_string)
+        expected = "TEST.test"
+
+        actual = log_item.file()
+
+        self.assertEqual(expected, actual)
+
+    def test_contains_referer(self):
+        log_item = LogItem(self.test_log_string)
+        expected = "http://test.com/tester"
+
+        actual = log_item.referrer()
+
+        self.assertEqual(expected, actual)
+
+
+# class LogSearchTests(unittest.TestCase):
 
 
 if __name__ == '__main__':
