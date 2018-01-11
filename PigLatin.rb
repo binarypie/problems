69e42104c654cc4e96d6071affa97f42f35779f1
Nikita Ahuja <nahuja038@gmail.com>
2018-01-10 19:35:04 -0600

    vowel transofmrer working and accounting for last character being a special char

--- PigLatin.rb
+++ PigLatin.rb
@@ -12,18 +12,23 @@ require 'pry'
 
 # Helper Methods
 
-def wordify(string)
-  # string_array = string.split( /\s+|\b/ )
-  string_array = string.split( /\s+|\!|\-|\.|\,/ )
+def wordify(sentence)
+  return sentence_array = sentence.split(" ")
+  # return sentence_array = sentence.split( /\s+|\b/ )
+  # return sentence_array = sentence.split( /\s+|\!|\-|\.|\,/ )
+  # p sentence_array = sentence.split( /\s+|\b|\W/ )
 end
 
 def change_consonant_starters(word)
   word_array = word.downcase.split("")
   first_letter = word_array.shift
-  # if word_array.include(a special character)
-  word_array << first_letter
-  new_word = word_array.join()
-  final_word = new_word + "ay"
+  # binding.pry
+  if /\W/.match(word_array[-1]).class == MatchData
+    final_word = word_array.insert(-2, first_letter+"ay").join
+  else
+    word_array << first_letter
+    final_word = word_array.join + "ay"
+  end
   if word[0] && word[0].capitalize === word[0]
     return final_word.capitalize!
   else
@@ -33,26 +38,45 @@ end
 
 def change_vowel_starters(word)
   word_array = word.split("")
+  if /\W/.match(word_array[-1]).class == MatchData
+    final_word = word_array.insert(-2, "way").join
+  else
+    final_word = word + "way"
+  end
 end
 
 def is_hyphenated?(word)
 end
 
-
-wordified = wordify("HeLLo World! I can't wait to explore your VAST forests. The-End!")
-
-vowel = ['a','e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-punctuation = ['!', '.', '?']
-
-wordified.each do |word|
-  # binding.pry
-  if vowel.include?(word[0])
-    puts "Vowerl word: #{word}"
-  elsif word == ""
-    puts "special char"
-  else
-    puts change_consonant_starters(word)
-  end
+def end_with_way?(word)
+  word[-3..-1] == "way"
 end
+
+
+# p wordified = wordify("HeLLo World! I can't wait to explore your VAST forests. The-End!")
+# p wordified.join("")
+
+puts change_consonant_starters("Hello!")
+puts change_vowel_starters("Apple!")
+
+
+# def pig_latinify(sentence)
+#   vowel = ['a','e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+#   wordify(sentence).each do |word|
+#     if vowel.include?(word[0])
+#       change_vowel_starters(word)
+#     else
+#       change_consonant_starters(word)
+
+# wordified.each do |word|
+#   # binding.pry
+#   if vowel.include?(word[0])
+#     puts "Vowel word: #{word}"
+#   elsif word == /\W/
+#     puts word
+#   else
+#     puts change_consonant_starters(word)
+#   end
+# end
 
 # p change_consonant_starters(wordified)
