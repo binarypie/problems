7a3d709d966a8b8e632795c4f46c0d5dcf3229c4
Nikita Ahuja <nahuja038@gmail.com>
2018-01-12 09:44:38 -0600

    complete pseudocode

--- CommonSubsequence.rb
+++ CommonSubsequence.rb
@@ -7,13 +7,16 @@ final_string = []
 string1 = string1.split("")
 string2 = string2.split("")
 
-# refer to the first character.
 
-# is the character present in second string?
 
-# if it is
 
-  # look at each string, beginning from the common letter. Store the common letter, and all following letters into an array.
+
+
+# refer to the first character in the first string argument.
+
+# if character is present in second string
+
+  # look at each string beginning from the common letter. Store the common letter, and all following letters into an array.
 
   # XMJYAUZ
   # array1 = ['X', 'M', 'J', 'Y', 'A', 'U', 'Z']
@@ -22,18 +25,19 @@ string2 = string2.split("")
     # array2 = ['X', 'U']
 
     # common_letters = array1 & array2
+    # => ['X', 'U']
 
       # if common_letters.join("").length > longest_common_sub.length
+
         # longest_common_sub = common_letters.join("")
+
       # else
+
         # longest_common_sub = longest_common_sub
+
       # end
 
-
-    # longest_common_sub = common_letters.join("")
-      # longest_common_sub = "XU"
-
-# else (if there is not x in the second string)
+# else (if character is not present in second string)
 
   # move on to the next character
 
