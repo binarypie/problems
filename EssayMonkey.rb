450534d5d1af3c203f11472322e93b0f7e22b6fd
Nikita Ahuja <nahuja038@gmail.com>
2018-01-11 22:49:53 -0600

    Accounts for greater human error -- pretty much omplete

--- EssayMonkey.rb
+++ EssayMonkey.rb
@@ -33,25 +33,33 @@ class Essay
 
 end
 
-puts "Welcome to the essay generator! How many paragraphs would you like your essay to have? Please enter an integer (i.e. 4)."
-input_1 = gets.chomp.to_i
 
-if input_1 == 0
-  puts "Ooh... looks like you didn't enter an integer or you entered 0! Let's try that again. Please enter an integer greater than 0 (i.e. 4)."
-  input_1 = gets.chomp.to_i
+######### RUNNER (CMD LINE) ############
+
+puts "Welcome to the essay generator! How many paragraphs would you like your essay to have? Please enter an integer (i.e. 4)."
+input_1 = gets.chomp
+
+if input_1.to_i == 0
+  until !(input_1.to_i == 0)
+    puts "Ooh... looks like you didn't enter an integer or you entered 0! Let's try that again. Please enter an integer greater than 0 (i.e. 4)."
+    input_1 = gets.chomp
+  end
+  num_p = input_1.to_i
 else
   num_p = input_1.to_i
 end
 
 puts "Great! Your essay will contain #{num_p} paragraphs. How many sentences would you like each paragraph to have? Please enter an integer (i.e. 5)."
+input_2 = gets.chomp
 
-input_2 = gets.chomp.to_i
-
-if input_2 == 0
-  puts "Ooh... looks like you didn't enter an integer or entered 0! Let's try that again! Please enter an integer greater than 0 (i.e. 4)."
-  input_2 = gets.chomp.to_i
+if input_2.to_i == 0
+  until !(input_2.to_i == 0)
+    puts "Ooh... looks like you didn't enter an integer or entered 0! Let's try that again! Please enter an integer greater than 0 (i.e. 4)."
+    input_2 = gets.chomp.to_i
+  end
+  num_s = input_2.to_i
 else
-  num_s = input_2
+  num_s = input_2.to_i
 end
 
 puts essay = Essay.new(num_p, num_s).generate
