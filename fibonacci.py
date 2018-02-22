fd9dcd3d264cfe4d41daae9982e652ac47ae9d94
Aishwarya Srivastava <aishwaryasrivastava@mail.com>
2018-02-21 21:47:43 -0500

    Fibonacci sequence solution

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+# Library imports
+import sys
+
+# Return the Fibonacci series up to :n as a string
+def F(n):
+	if n < 3:
+		return ["0","1"][:n]
+	else:
+		seq = F(n-1)
+		Fn_1 = int(seq[-1])
+		Fn_2 = int(seq[-2])
+		Fn = Fn_1 + Fn_2 
+		return seq + [str(Fn)]
+
+n = int(sys.argv[1])
+print(', '.join(F(n)))
