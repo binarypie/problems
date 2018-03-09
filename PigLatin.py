ec1febd5666b59942e8f01f754ede47cc52e1c77
EC2 Default User <ec2-user@ip-172-31-27-211.us-east-2.compute.internal>
2018-03-09 00:40:00 +0000

    cleaned up flow, added user input

--- PigLatin.py
+++ PigLatin.py
@@ -1,14 +1,3 @@
-# coding: utf-8
-# Words that start with a consonant have their first letter moved to the end of the word and the letters “ay” added to the end.
-# Words that start with a vowel have the letters “way” added to the end.
-# Words that end in “way” are not modified.
-# Punctuation must remain in the same relative place from the end of the word.
-# Hyphens are treated as two words
-# Capitalization must remain in the same place.
-# Single letters are not modified.
-
-# result = "ElLOhay Orldway! I antca'y aitway otay exploreway ouryay ASTVay orestfay. Hetay-Endway!"
-
 def loop(word, hyphen):
     
     result = ""
@@ -37,21 +26,22 @@ def loop(word, hyphen):
         
     return result
 
-string = "Hello world!"
-bigstring = "HeLLo World! I can't wait to explore your VAST forests. The-End!"
-
-vowels = ['a', 'e', 'i', 'o', 'u']
-result = ""
-
-words = bigstring.split(' ')
-
-for word in words:
-    if "-" in word:
-        more_words = word.split("-")
-        for new_word in more_words:
-            result += loop(new_word, True)
-    else:
-        result += loop(word, False)
+if __name__ == "__main__":
     
-print(result)
+    input_string = raw_input("Please enter a string to convert: ")
+    
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    result = ""
+
+    words = input_string.split(' ')
+    
+    for word in words:
+        if "-" in word:
+            more_words = word.split("-")
+            for new_word in more_words:
+                result += loop(new_word, True)
+        else:
+            result += loop(word, False)
+        
+    print(result)
     
